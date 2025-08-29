0c8644d5d5e7e29be2756ce96f5c09346814b23b
Renan <renan13.com@gmailcom>
2025-08-29 20:22:27 -0300

    Adicionando versao de conectar usuarios

--- app.js
+++ app.js
@@ -16,22 +16,36 @@ serverHttp.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
 
+let users = [];
 Io.on("connection", (socket) => {
+  socket.on("joinRoom", (name) => {
+    let isUser = users.find((user) => user.name === name);
+    if (!isUser) {
+      users.push({
+        name: name,
+        id: socket.id,
+      });
+      Io.emit("updateUsers", {newUser: name, allUsers: users});
+    } else {
+      isUser.id = socket.id;
+    }
+  });
+
   console.log(`Usuário conectado no servidor: ${socket.id}`);
   let cont = 0;
   let interval = null;
 
   socket.on("startTimer", () => {
-    
-    if (interval) return
-      interval = setInterval(() => {
-        cont += 1;
-        socket.emit("setSeconds", cont);
-      }, 1000);
+    if (interval) return;
+    interval = setInterval(() => {
+      cont += 1;
+      socket.emit("setSeconds", cont);
+    }, 1000);
   });
 
   socket.on("disconnect", () => {
     console.log(`Usuário desconectado do servidor: ${socket.id}`);
+    users = users.filter((user) => user.id != socket.id);
     clearInterval(interval);
   });
 });
